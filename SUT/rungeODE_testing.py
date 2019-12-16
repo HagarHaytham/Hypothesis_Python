0f466029f245cdba20fb41cb67d1e46744241ad2
OmniiaMohyee <zicomohyee@yahoo.com>
2019-12-16 21:28:35 +0200

    add runge kutte

--- SUT/rungeODE_testing.py
+++ SUT/rungeODE_testing.py
@@ -2,36 +2,23 @@ from hypothesis import given
 import hypothesis.strategies as st
 import numpy as np
 import hypothesis.extra.numpy as hnp
-from hypothesis import assume
+from hypothesis import assume, settings
 
 
 from rungeODE import Runge_kutte_ODE
 
 #NOTE : can't generate an equation string.
-#NOTE : this type of problems can't 
-@given(s=st.text())
-def test_Runge_kutte_ODE(s):
+#NOTE : this type of problems can't be solved using generated input, because of the need for customized input.
+
+@given(x = st.integers())
+
+def test_Runge_kutte_ODE(x):
 
     r = Runge_kutte_ODE(0,2,0,0.5,"4*(exp(0.8*x))-0.5 * y","5* (exp(0.8*x))-0.25 * (y**2)")
-    print(r.ya)
-    assert r.ya == r.ya
-    pass
-
-def randomize_size():
-    return st.integers(1,10)
-
-
-@given( l= hnp.arrays(np.integer,hnp.array_shapes(min_dims=2, max_dims=2) ,elements=st.integers(1,100),unique = True))
-def test_rand(l):
-    assume(l.shape[1] == 2)
-    print(l.shape)
-    
-
-    pass
+    output = "{:.4f}".format(r.ya)
+    assert float(output) == 3.7517
 
 
 if __name__ == "__main__":
-    #test_Runge_kutte_ODE()
-    #print(randomize_size())
-    test_rand()
-    #print(i)
+    test_Runge_kutte_ODE()
+    
